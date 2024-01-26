afb9e7bcf9a5fa5d665d3fcb249ae312c11f13a9
VicentChoi3449 <bigchoi3449@gmail.com>
2024-01-26 16:49:33 +0900

    Implement valid train

--- develop/choi/train.py
+++ develop/choi/train.py
@@ -14,9 +14,10 @@ from tqdm import tqdm
 from east_dataset import EASTDataset
 from dataset import SceneTextDataset
 from model import EAST
-
 import numpy as np
 np.random.seed(2024)
+
+import wandb
 
 def parse_args():
     parser = ArgumentParser()
@@ -34,7 +35,7 @@ def parse_args():
     parser.add_argument('--input_size', type=int, default=1024)
     parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
-    parser.add_argument('--max_epoch', type=int, default=1)
+    parser.add_argument('--max_epoch', type=int, default=300)
     parser.add_argument('--save_interval', type=int, default=5)
     parser.add_argument('--ignore_tags', type=list, default=['masked', 'excluded-region', 'maintable', 'stamp'])
 
@@ -48,6 +49,18 @@ def parse_args():
 
 def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
                 learning_rate, max_epoch, save_interval, ignore_tags):
+    # best_loss = float('inf')
+    wandb.init(
+    # Set the project where this run will be logged
+    project="choi", name='first_trial',
+    # Track hyperparameters and run metadata
+    config={
+        "learning_rate": args.learning_rate,  # 학습률을 wandb config에 추가
+        "epochs": args.max_epoch,
+    },
+    )
+
+    # 훈련 데이터셋 load
     dataset = SceneTextDataset(
         data_dir,
         split='train',
@@ -64,15 +77,40 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
         num_workers=num_workers
     )
 
+    # 검증 데이터셋 로드
+    valid_dataset = SceneTextDataset(
+        data_dir,
+        split='valid',  # 'valid' 스플릿 사용
+        image_size=image_size,
+        crop_size=input_size,
+        ignore_tags=ignore_tags
+    )
+    valid_dataset = EASTDataset(valid_dataset)
+    _num_batches = math.ceil(len(valid_dataset) / batch_size)
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers
+    )
+
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EAST()
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
+    print('cuda 사용중 :', torch.cuda.is_available())
+    #optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-2, amsgrad=False)
+    #optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, cycle_momentum=True)
+    #scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
+    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 3, max_epoch//5*4], gamma=0.2)
+    #scheduler = lr_scheduler.CyclicLR(optimizer, base_lr=learning_rate, max_lr = learning_rate*100, step_size_up=10, mode='triangular2')
 
-    model.train()
+
+    #model.train()
     for epoch in range(max_epoch):
-        epoch_loss, epoch_start = 0, time.time()
+        model.train()
+        epoch_loss, epoch_start, _epoch_loss = 0, time.time(), 0
         with tqdm(total=num_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
                 pbar.set_description('[Epoch {}]'.format(epoch + 1))
@@ -87,21 +125,58 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
 
                 pbar.update(1)
                 val_dict = {
-                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                    'IoU loss': extra_info['iou_loss']
+                    't_Cls loss': extra_info['cls_loss'], 't_Angle loss': extra_info['angle_loss'],
+                    't_IoU loss': extra_info['iou_loss']
                 }
                 pbar.set_postfix(val_dict)
 
+
+        model.eval()
+        # valid_loss = 0
+        with tqdm(total=_num_batches) as _pbar:
+            with torch.no_grad():
+                for img, gt_score_map, gt_geo_map, roi_mask in valid_loader:
+                    _loss, _extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                    # _loss_val = _loss.item()
+                    # _epoch_loss += _loss_val
+
+                    _pbar.update(1)
+                    _valid_dict = {
+                        'v_Cls loss': _extra_info['cls_loss'], 'v_Angle loss': _extra_info['angle_loss'],
+                        'v_IoU loss': _extra_info['iou_loss']
+                    }
+                    _pbar.set_postfix(_valid_dict)
+
+        wandb.log({'learning_rate': optimizer.param_groups[0]['lr'],
+                   't_Cls loss': extra_info['cls_loss'],
+                   't_Angle loss': extra_info['angle_loss'],
+                    't_IoU loss': extra_info['iou_loss'],
+                    "v_Cls loss": _extra_info['cls_loss'],
+                    "v_Angle loss": _extra_info['angle_loss'],
+                    'v_IoU loss': _extra_info['iou_loss']})
+        
+        # 훈련 및 검증 메트릭을 모두 포함하여 출력
+        #print(f'Epoch {epoch + 1} - Train Loss: {_epoch_loss / num_batches:.4f}, 'f'Valid Loss: {valid_loss:.4f}, {_valid_dict}')
+
+        #print(f'Epoch {epoch+1}, Valid Loss: {valid_loss:.4f}')
+
+        # 체크포인트 저장 조건
+        # if valid_loss < best_loss:
+        #     best_loss = valid_loss
+        #     ckpt_fpath = osp.join(model_dir, 'best_model.pth')
+        #     torch.save(model.state_dict(), ckpt_fpath)
         scheduler.step()
 
         print('Mean loss: {:.4f} | Elapsed time: {}'.format(
             epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
 
         if (epoch + 1) % save_interval == 0:
+            print('Vlid loss: {:.4f} | Elapsed time: {}'.format(
+                _epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
             if not osp.exists(model_dir):
                 os.makedirs(model_dir)
 
-            ckpt_fpath = osp.join(model_dir, 'latest.pth')
+            ckpt_fpath = osp.join(model_dir, f'{epoch+1}.pth')
             torch.save(model.state_dict(), ckpt_fpath)
 
 
